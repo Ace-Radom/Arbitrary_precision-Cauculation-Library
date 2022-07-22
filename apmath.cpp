fe6311f2f995719236abb91aa08bd2af26e427e0
Sichen Lyu <104905842+Ace-Radom@users.noreply.github.com>
2022-07-22 17:29:37 +0200

    Complete integer DTchange function

--- apmath.cpp
+++ apmath.cpp
@@ -1,5 +1,13 @@
 #include"apmath.h"
 
-char *DTchagne( long long Integer = 0LL ){
-
+/* change one integer into char* */
+std::string DTchange( long long __Integer = 0LL ){
+    std::string _ANT = "";
+    while ( __Integer != 0 )
+    {
+        char _tc = ( char ) ( __Integer % 10 ) + 48;
+        _ANT.insert( 0 , 1 , _tc );
+        __Integer /= 10;
+    }
+    return _ANT;
 }
--- apmath.h
+++ apmath.h
@@ -1,11 +1,13 @@
 #ifndef _APMATH_H_
 #define _APMATH_H_
 
+#include<string>
+
 #include"src/apaddition.h"
 #include"src/apsubstraction.h"
 #include"src/apmultiplication.h"
 #include"src/apdivision.h"
 
-char *DTchagne( long long = 0LL );
+std::string DTchange( long long = 0LL );
 
 #endif
