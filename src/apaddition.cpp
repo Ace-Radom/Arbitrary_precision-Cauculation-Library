2a791d08da249aa6418b474059fd200a8ce9ce56
Sichen Lyu <104905842+Ace-Radom@users.noreply.github.com>
2022-07-22 20:30:28 +0200

    Add files via upload

---
+++ src/apaddition.cpp
@@ -0,0 +1,5 @@
+#include"apaddition.h"
+
+std::string apadd( std::string __NUM1, std::string __NUM2 , const uint8_t __digit = 1 ){
+    
+}
---
+++ src/apaddition.h
@@ -0,0 +1,8 @@
+#ifndef _APADDITION_H_
+#define _APADDITION_H_
+
+#include<string>
+
+std::string apadd( std::string , std::string , const uint8_t );
+
+#endif
