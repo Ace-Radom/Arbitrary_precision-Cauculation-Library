895017ce2c2a3daadcc01b5dba5e89957508db9e
Sichen Lyu <104905842+Ace-Radom@users.noreply.github.com>
2022-07-22 20:30:43 +0200

    Add files via upload

--- apmath.h
+++ apmath.h
@@ -2,12 +2,11 @@
 #define _APMATH_H_
 
 #include<string>
-
 #include"src/apaddition.h"
 #include"src/apsubstraction.h"
 #include"src/apmultiplication.h"
 #include"src/apdivision.h"
 
-std::string DTchange( long long = 0LL );
+std::string DTchange( long long );
 
 #endif
